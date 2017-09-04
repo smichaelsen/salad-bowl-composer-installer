718037d32e4f206702478a354da2ae0d1b755169
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-09-04 10:40:57 +0200

    Work on composer plugin

--- Classes/PluginInstaller.php
+++ Classes/PluginInstaller.php
@@ -5,7 +5,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class PluginInstaller extends PluginInterface
+class PluginInstaller implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
